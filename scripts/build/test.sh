f7a6de2cff07b35d674d03106e53094d0cd2de73
Aliaksei Syrel <alex.syrel@gmail.com>
2019-01-10 14:17:57 +0100

    Update test.sh
    run tests and examples in all Bloc* packages

--- scripts/build/test.sh
+++ scripts/build/test.sh
@@ -2,6 +2,6 @@
 set -o xtrace
 echo $DISPLAY
 export DISPLAY=:99.0
-./pharo Pharo.image examples --junit-xml-output 'Bloc-Tests.*' 2>&1
-./pharo Pharo.image test --junit-xml-output 'Bloc-Tests.*' 2>&1
+./pharo Pharo.image examples --junit-xml-output 'Bloc.*' 2>&1
+./pharo Pharo.image test --junit-xml-output 'Bloc.*' 2>&1
 exit 0
